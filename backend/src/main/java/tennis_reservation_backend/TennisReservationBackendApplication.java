e90e4af8e74c660f9d79ab029d49913826433765
rtsugihara-hub <r.tsugihara@inswave.jp>
2026-08-27 16:44:14 +0900

    feat: backendにSpring Bootの初期プロジェクトを追加

---
+++ backend/src/main/java/tennis_reservation_backend/TennisReservationBackendApplication.java
@@ -0,0 +1,13 @@
+package tennis_reservation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisReservationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisReservationBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tennis_reservation_backend/TennisReservationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tennis_reservation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisReservationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
